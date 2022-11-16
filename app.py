88d5fcf2f4858f50b758531f0e040607d9e9c44c
1Tsw0rd <31820402+SeDevel@users.noreply.github.com>
2022-11-16 10:50:39 +0900

    1. add_1st code
    app.py
    crawling.html
    
    초기코드

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
+#import requests 크롤링 라이브러리 (정적)requests
+# html 예쁘게 긁어오기 bs4
+from bs4 import BeautifulSoup
+# 동적페에지 크롤링 selenium
+from selenium import webdriver
 
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://ID:PW@clusterName.spvewgv.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+#flask 임포트
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('crawling.html')
 
-@app.route("/sample", methods=["POST"])
-def sample_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-    doc = {'name': name_receive, 'comment': comment_receive}
-    db.fancomments.insert_one(doc)
+@app.route('/crawling', methods=["POST"])
+def web_crawling_youtube():
+    #### 1. 사용자 키워드 및 크롤링 대상 url 세팅 ####
+    keyword = '건강'
+    target_url = 'https://www.youtube.com/results?search_query=' + keyword
 
-    return jsonify({'msg': '작성 완료!'})
+    #### 2. 동적페이지 크롤링 ####
+    # 옵션 생성
+    options = webdriver.ChromeOptions()
+    # 창 숨기는 옵션 추가(백그라운드로 실행, 이걸 하지 않으면 브라우저 열어서 탐색하게 됨)
+    options.add_argument("headless")
 
-@app.route("/sample", methods=["GET"])
-def sample_get():
-    cheer_comments = list(db.fancomments.find({}, {'_id': False}))
-    return jsonify(cheer_comments)
+    # driver 실행
+    driver = webdriver.Chrome(options=options)
+    driver.get(target_url)
+
+    #html 가져오기
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # driver 종료
+    driver.quit()
+
+    #### 3. 링크주소 추출 및 json 배열화 ####
+    youtube_links = []
+    if soup != None:
+        i = 0
+        j = 0
+        for i in  range(0, 3, 1):
+            j += 1
+            crawling_link = soup.select_one('#contents > ytd-video-renderer:nth-child('+str(j)+') > #dismissible > ytd-thumbnail > #thumbnail')['href'].strip()
+            crawling_link = crawling_link.replace('/watch?v=', '') #링크 식별값만 추출
+            youtube_links.append({i:crawling_link}) #append : 배열 뒤로 추가
+
+    return jsonify({'ytb_links':youtube_links})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=3000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
+
+#<iframe width="560" height="315" src="https://www.youtube.com/embed/M3KWNLf3Mm0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
